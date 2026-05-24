acf569252b89c8e2e8676ce3713d0b33372793ec
lawrencenjeri4-lgtm <lawrencenjeri4@gmail.com>
2026-05-24 22:40:21 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,29 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center px-6 py-5 border-b border-cyan-500/20 bg-black sticky top-0 z-50">
+      
+      <h1 className="text-2xl font-bold text-cyan-400">
+        Lucid Tech Solutions 🚀
+      </h1>
 
+      <div className="hidden md:flex gap-6 text-gray-300">
+        <a href="#" className="hover:text-cyan-400 transition">
+          Home
+        </a>
+
+        <a href="#services" className="hover:text-cyan-400 transition">
+          Services
+        </a>
+
+        <a href="#projects" className="hover:text-cyan-400 transition">
+          Projects
+        </a>
+
+        <a href="#contact" className="hover:text-cyan-400 transition">
+          Contact
+        </a>
+      </div>
+
+    </nav>
+  )
+}
