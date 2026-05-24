5858cbe292ef51ec81ec3a7539a362e517dd029c
lawrencenjeri4-lgtm <lawrencenjeri4@gmail.com>
2026-05-24 22:44:37 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-cyan-500/20 bg-black py-6 text-center text-gray-500">
 
+      <p>
+        © 2026 Lucid Tech Solutions. All rights reserved.
+      </p>
+
+    </footer>
+  )
+}
